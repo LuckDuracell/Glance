d32998106126c8f04834703e86db0a4f899cfd61
LuckDuracell <lukedru@yahoo.com>
2021-07-05 19:58:38 -0700

    Initial Commit

---
+++ Glance/GlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceApp.swift
+//  Glance
+//
+//  Created by Luke Drushell on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
